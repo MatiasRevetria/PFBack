2ebd3ffc1ea6d6a9cb3c9fab1f1931cc5b04ff75
matiasrevetria <revetriamatias03@gmail.com>
2024-07-10 19:16:53 -0300

    pusho

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 const express = require("express");
 const modulos = require("./modulos");
+const path = require("path");
 
 const app = express();
 
+app.use(express.static(path.join(__dirname,"public")));
 
 app.get("/",(req,res)=>{
     res.send("Hola Express");
-})
+});
 
+app.get("/factura", (req,res) => {
+    res.sendFile(path.join(__dirname,'private', 'factura.html'));
+});
 
-let PORT = 3000;
-app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
+let PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server is running on http://localhost:${PORT}`));
